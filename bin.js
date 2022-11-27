f0f21a2fdc1d8417d87e713d28a08831a54ba4ce
Dominic Tarr <dominic.tarr@gmail.com>
2022-11-28 11:55:24 +1300

    options to output the whole tree

--- bin.js
+++ bin.js
@@ -1,10 +1,25 @@
 var TreeHash = require('./')
+var TreeHashFlat = require('./flat')
+var opts = require('minimist')(process.argv.slice(2))
+var {height} = require('./util')
+var input_name = opts._.shift()
+if(input_name == '.' || !input_name)
+  input = process.stdin
+else
+  input = require('fs').createReadStream(input_name)
 
-var th = new TreeHash()
-
-process.stdin.on('data', (data) => {
-  th.update(data)
-})
-.on('end', () => {
-  console.log(th.digest().toString('hex'))
-})
+var isTree = opts.tree || opts.t
+var th = isTree ? new TreeHashFlat() : new TreeHash()
+  input.on('data', (data) => {
+    th.update(data)
+  })
+  .on('end', () => {
+    if(isTree) {
+      th.digest()
+      for(var i = 0; i < th.tree.length; i++) {
+        console.log(height(i), th.tree[i])
+      }
+    }
+    else
+      console.log(th.digest().toString('hex'))
+  })
