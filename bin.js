79691a8c3a2395302302483de8524deb5968b943
Dominic Tarr <dominic.tarr@gmail.com>
2022-12-05 21:21:22 +1300

    pretty print tree

--- bin.js
+++ bin.js
@@ -2,7 +2,16 @@ var TreeHash = require('./')
 var TreeHashFlat = require('./flat')
 var opts = require('minimist')(process.argv.slice(2))
 var {height} = require('./util')
+var {print_tree} = require('./print')
 var input_name = opts._.shift()
+
+if(opts.help) {
+  console.error('usage:')
+  console.error('treehash <filename>|. {--tree,--line_numbers?}')
+  console.error()
+  console.error('  --tree           # pretty print the tree hash as a tree')
+  console.error('    --branch_numbers # modifies --tree to include branch number of the tree.') 
+}
 if(input_name == '.' || !input_name)
   input = process.stdin
 else
@@ -16,10 +25,7 @@ var th = isTree ? new TreeHashFlat() : new TreeHash()
   .on('end', () => {
     if(isTree) {
       var h = th.digest()
-      for(var i = 0; i < th.tree.length; i++) {
-        console.log(height(i), th.tree[i])
-      }
-      console.log(h.toString('hex'))
+      console.log(print_tree(th.tree, opts))
     }
     else
       console.log(th.digest().toString('hex'))
