102402ddfa6a8cada7e6e03077fc1747282f4b83
Dominic Tarr <dominic.tarr@gmail.com>
2022-12-12 20:40:22 +1300

    output proof in json, we aren't here to write custom parsers

--- bin.js
+++ bin.js
@@ -3,38 +3,89 @@ var TreeHashFlat = require('./flat')
 var opts = require('minimist')(process.argv.slice(2))
 var {height, block2leaf_index} = require('./util')
 var {print_tree} = require('./print')
+var cmd = opts._.shift()
 var input_name = opts._.shift()
-
-if(opts.help) {
+var input
+var value = opts._.shift()
+var block_size = 1024*1024
+var ALG = 'sha256'
+if(cmd === 'help' || opts.help) {
   console.error('usage:')
-  console.error('treehash <filename>|. {--tree,--line_numbers?}')
+  console.error('treehash <command> <filename>|. {--tree,--line_numbers?}')
   console.error()
-  console.error('  --tree           # pretty print the tree hash as a tree')
+  console.error('  tree           # pretty print the tree hash as a tree')
   console.error('    --branch_numbers # modifies --tree to include branch number of the tree.') 
 }
-if(input_name == '.' || !input_name)
-  input = process.stdin
-else
-  input = require('fs').createReadStream(input_name)
 
-var isTree = opts.tree || opts.t
-var isProof = opts.proof || opts.p
+const
+  TREE = 'tree',
+  PROOF = 'proof',
+  BLOCK = 'block',
+  HASH = 'hash',
+  VERIFY = 'verify',
+  APPEND = 'append'
+
+var isTree = cmd === TREE
+var isProof = cmd === PROOF
 var th = isTree || isProof ? new TreeHashFlat() : new TreeHash()
-  input.on('data', (data) => {
-    th.update(data)
+
+var block = cmd === BLOCK ? opts.block || value : 0
+
+var start = block * block_size
+//note, end is inclusive, so need - 1 there
+var end   = cmd === BLOCK ? ((block+1) * block_size) - 1 : Infinity
+
+if (cmd === BLOCK) {
+  if(input_name == '.' || !input_name) {
+    console.error('block command requires filename input')
+    process.exit(1)
+  }
+  else
+    input = require('fs').createReadStream(input_name, { start, end })
+  return input.pipe(process.stdout)
+}
+else {
+  if(input_name == '.' || !input_name)
+    input = process.stdin
+  else
+    input = require('fs').createReadStream(input_name, { start, end })
+}
+
+if (cmd === VERIFY) {
+  //open file
+  input.on('data', (d) => {
+    th.update(d)
   })
   .on('end', () => {
-    if(isTree || isProof) {
-      var h = th.digest()
-      if(isTree)
-        console.log(print_tree(th.tree, opts))
-      else if(isProof) {
-        var leaf_index = block2leaf_index(opts.proof)
-        console.log('root:', th.digest().toString('hex')+';')
-        console.log('proof:', th.proof(leaf_index).map(e=>e.toString('hex')).join(','))
-      }
-    }
-    else
-      console.log(th.digest().toString('hex'))
+
 
   })
+
+
+  return
+}
+
+input.on('data', (data) => {
+  th.update(data)
+})
+.on('end', () => {
+  if(isTree || isProof) {
+    var h = th.digest()
+    if(isTree)
+      console.log(print_tree(th.tree, opts))
+    else if(isProof) {
+      var block_index = ~~+(opts.proof || value)
+      var leaf_index = block2leaf_index(opts.proof || value)
+      console.log(JSON.stringify({
+        index: leaf_index,
+        root: th.digest().toString('hex'),
+        proof: th.proof(leaf_index).map(e=>e.toString('hex')),
+        alg: ALG,
+        block_size
+      }, null, 2))
+    }
+  }
+  else
+    console.log(th.digest().toString('hex'))
+
+})
